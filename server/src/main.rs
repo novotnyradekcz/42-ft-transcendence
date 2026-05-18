19eba3025c1d3ace2ab0f965a124bfc0a4e08b1e
czarte <czarte@gmail.com>
2026-05-18 14:45:42 +0200

    running version on 1.95

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,11 @@
-use actix_web::{web, App, HttpResponse, HttpServer}
-use std::io::Result
+// Copyright (c) 2018, ft_transcendence (https://42.fr) and/or its affiliates. All rights reserved.
+
+use actix_web::{web, App, HttpResponse, HttpServer};
+use std::io::Result;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(HttpResponse::Ok))).
+    HttpServer::new(|| App::new().route("/", web::get().to(HttpResponse::Ok)))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
