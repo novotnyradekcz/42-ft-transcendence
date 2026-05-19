d85f52a3edece2e3ebf7e045f0a4cdd16b608acd
Vojtech Parkan <voparkan@c3r3s3.42prague.com>
2026-05-19 15:08:08 +0200

    add openssl support

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,19 @@
 // Copyright (c) 2018, ft_transcendence (https://42.fr) and/or its affiliates. All rights reserved.
 
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 use std::io::Result;
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+
+#[get("/")]
+async fn index(_req: HttpRequest) -> impl Responder {
+    "Welcome"
+}
 
 #[actix_web::main]
 async fn main() -> Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(HttpResponse::Ok)))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    let mut builder = SslAcceptor::mozzila_intermediate(SslMethod::tls()).unwrap();
+    builder.set_private_key_file("key.pem", SslFileType).unwrap();
+    builder.ssl_certificate_chain_file("cert.pem").unwrap();
+
+    HttpServer::new(|| App::new().service(index)).bind_openssl("127.0.0.1:8080", builder).run().await
 }
