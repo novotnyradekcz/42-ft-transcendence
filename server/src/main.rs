4a2443c653faf60570b84464e989d772e4c02783
Vojtech Parkan <voparkan@c3r3s3.42prague.com>
2026-05-19 16:47:27 +0200

    remove ssl support - not needed

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,19 +1,24 @@
 // Copyright (c) 2018, ft_transcendence (https://42.fr) and/or its affiliates. All rights reserved.
 
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{App, HttpServer, Responder, web};
 use std::io::Result;
-use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+use std::io::Error;
+use serde::{Serialize, Deserialize};
 
-#[get("/")]
-async fn index(_req: HttpRequest) -> impl Responder {
-    "Welcome"
+#[derive(Serialize, Deserialize)]
+struct Info {
+    user_ud: u32,
+    frien: String,
+}
+
+async fn index(path: web::Path<(String, String)>, json: web::Json<Info>) -> impl Responder {
+    Ok::<String, Error>("Welcome".to_owned() + &path.1 + json.to_string())
 }
 
 #[actix_web::main]
 async fn main() -> Result<()> {
-    let mut builder = SslAcceptor::mozzila_intermediate(SslMethod::tls()).unwrap();
-    builder.set_private_key_file("key.pem", SslFileType).unwrap();
-    builder.ssl_certificate_chain_file("cert.pem").unwrap();
-
-    HttpServer::new(|| App::new().service(index)).bind_openssl("127.0.0.1:8080", builder).run().await
+    HttpServer::new(|| App::new().service(index))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
